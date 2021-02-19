1a4ca3a50e4dbce7fb384f750ad90bbae92f5851
Ibrahim <ibrahimfayyaz26@gmail.github.com>
2021-02-20 02:46:38 +0500

    trending

--- Screens/Home.js
+++ Screens/Home.js
@@ -11,35 +11,37 @@ import Product from "../components/ProductCategories";
 
 const Home = (props) => {
   return (
-    <ScrollView style={{ flex: 1 }}>
-      <Product
-        press={() => {
-          props.navigation.navigate("Categories", {
-            itemName: "SPORTS WEARS & SPORTS UNIFORM",
-          });
-        }}
-        imgSource={require("../assets/work1.jpg")}
-        text={"SPORTS WEARS & SPORTS UNIFORM"}
-      />
-      <Product
-        press={() =>
-          props.navigation.navigate("Categories", {
-            itemName: "MARTIAL ARTS",
-          })
-        }
-        imgSource={require("../assets/work2.jpg")}
-        text={"MARTIAL ARTS"}
-      />
-      <Product
-        press={() => {
-          props.navigation.navigate("Categories", {
-            itemName: "OTHER PRODUCTS",
-          });
-        }}
-        imgSource={require("../assets/work3.jpg")}
-        text={"OTHER PRODUCTS"}
-      />
-    </ScrollView>
+    <View style={{ flex: 1 }}>
+      <ScrollView style={{ flex: 1 }}>
+        <Product
+          press={() => {
+            props.navigation.navigate("Categories", {
+              itemName: "SPORTS WEARS & SPORTS UNIFORM",
+            });
+          }}
+          imgSource={require("../assets/work1.jpg")}
+          text={"SPORTS WEARS & SPORTS UNIFORM"}
+        />
+        <Product
+          press={() =>
+            props.navigation.navigate("Categories", {
+              itemName: "MARTIAL ARTS",
+            })
+          }
+          imgSource={require("../assets/work2.jpg")}
+          text={"MARTIAL ARTS"}
+        />
+        <Product
+          press={() => {
+            props.navigation.navigate("Categories", {
+              itemName: "OTHER PRODUCTS",
+            });
+          }}
+          imgSource={require("../assets/work3.jpg")}
+          text={"OTHER PRODUCTS"}
+        />
+      </ScrollView>
+    </View>
   );
 };
 
--- components/HeaderSuggestion.js
+++ components/HeaderSuggestion.js
@@ -1,26 +1,67 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Image,
+} from "react-native";
 
-const HeaderSuggestion = () => {
+const HeaderSuggestion = (props) => {
   return (
-    <View style={styles.container}>
-      <Text>HeaderSuggestion</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={props.press}>
+      <View style={styles.container}>
+        <View style={styles.imgContainer}>
+          <Image style={styles.img} source={{ uri: props.imageUri }} />
+        </View>
+        <View style={styles.textContainer}>
+          <Text style={{ fontSize: 18, fontStyle: "italic" }}>
+            {props.title}
+          </Text>
+          <Text style={styles.text}>{props.code}</Text>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
-    margin: 20,
-    borderRadius: 10,
-    shadowColor: "black",
-    shadowRadius: 8,
+    margin: 10,
+    marginLeft: 32,
+    borderRadius: 20,
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
+    shadowColor: "black",
+    shadowRadius: 20,
     elevation: 5,
     height: 200,
-    width: "70%",
+    width: 330,
+    flexDirection: "row",
+  },
+  imgContainer: {
+    width: "65%",
+    height: "100%",
+    borderTopLeftRadius: 20,
+    borderBottomLeftRadius: 20,
+    overflow: "hidden",
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+  },
+  textContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "35%",
+    height: "100%",
+  },
+  text: {
+    fontFamily: "Bold",
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
   },
 });
 
